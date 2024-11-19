8a5b338faf3e1b7d1cb2fdf666afe1648b010e40
Sarah Warren <njr4ss@virginia.edu>
2024-11-18 23:11:43 -0500

    activating/deactivating game with menu buttons

--- Assets/Scripts/CanvasManager.cs
+++ Assets/Scripts/CanvasManager.cs
@@ -18,6 +18,7 @@ public class CanvasManager : MonoBehaviour
     List<CanvasController> canvasControllerList;
     CanvasController lastActiveCanvas;
     public static CanvasManager Instance { get; private set; }
+    public Camera gameCamera;
     protected void Awake()
     {
         if (Instance != null && Instance != this)
@@ -44,6 +45,37 @@ public class CanvasManager : MonoBehaviour
         {
             newCanvas.gameObject.SetActive(true);
             lastActiveCanvas = newCanvas;
+            if (_type == CanvasType.GameUI)
+            {
+                ActivateGame();
+            } else if (_type == CanvasType.EndScreen)
+            {
+                DeactivateGame();
+            }
         } else {Debug.LogWarning("newCanvas is null");}
+    }
+    private void ActivateGame()
+    {
+        if (gameCamera != null)
+        {
+            gameCamera.enabled = true;
+        }
+        ResetGameElements();
+        Debug.Log("Game is active");
+    }
+
+
+    private void DeactivateGame()
+    {
+        Debug.Log("Game is inactive");
+        // TODO: implement this, not sure if camera needs to be deactivated 
+        // if (gameCamera != null)
+        // {
+        //     gameCamera.enabled = false;
+        // }
+    }
+        private void ResetGameElements()
+    {
+        Debug.Log("Need to reset game elements");
     }
 }
